55fc9f68c0ed92dc71c5ff80e6c2d5e791a89a27
romain <romain.dortier@gmail.com>
2017-02-27 23:27:26 +0100

    Motif

--- web/index.php
+++ web/index.php
@@ -10,10 +10,10 @@ $log = new Logger('name');
 $log->pushHandler(new StreamHandler('log.log', Logger::WARNING));
 
 // add records to the log
-$log->info(var_export('index', true));
-$log->info(var_export($_POST, true));
-// $log->warning('Foo');
-// $log->error('Bar');
+// $log->info(var_export('index', true));
+// $log->info(var_export($_POST, true));
+$log->warning('Foo');
+$log->error('Bar');
 
 header('Content-type:application/json;charset=utf-8');
 print '{
