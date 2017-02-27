afe1850505accf1920618c32dd0ec7f7e5b85113
romain <romain.dortier@gmail.com>
2017-02-27 23:47:22 +0100

    Test

--- web/index.php
+++ web/index.php
@@ -7,20 +7,23 @@ use Monolog\Handler\StreamHandler;
 
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler('log.log', Logger::WARNING));
+$log->pushHandler(new StreamHandler('log.log', Logger::INFO));
 
 // add records to the log
 // $log->info(var_export('index', true));
 // $log->info(var_export($_POST, true));
-$log->warning('SnneFoo');
-$log->error('Bar');
-$log->info(serialize($_GET));
-$log->info(serialize($_POST));
+// $log->warning($_GET['getparam']);
+$log->info('GET');
+$log->error(serialize($_GET));
+$log->info('POST');
+$log->error(serialize($_POST));
 
 
 header('Content-type:application/json;charset=utf-8');
-print '"fulfillment": {
-  "speech": "Today in Boston: Fair, the temperature is 37 F",
-  "source": "apiai_webhook",
-  "displayText": "Today in Boston: Fair, the temperatuuuure is 37 F"
-}';
+
+$data['fulfillment'] = 
+	['speech'=> 'Today in Boston: Fair, the temperature is 37 F',
+	'source'=> 'apiai_webhook',
+	'displayText'=> 'Today in Boston: Fair, the temperature is 37 F'
+	];
+print  json_encode($data);
