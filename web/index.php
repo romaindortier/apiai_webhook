b266bf383092ba405377b77fda74063d332c07cc
romain <romain.dortier@gmail.com>
2017-02-28 16:05:31 +0100

    Eee

--- web/index.php
+++ web/index.php
@@ -10,6 +10,12 @@ $log = new Logger('name');
 $log->pushHandler(new StreamHandler('log.log', Logger::INFO));
 
 header('Content-type:application/json;charset=utf-8');
+
+
+$json = file_get_contents('php://input'); 
+$request = json_decode($json, true);
+$action = $request["result"]["action"];
+$parameters = $request["result"]["parameters"];
 // add records to the log
 // $log->info(var_export('index', true));
 // $log->info(var_export($_POST, true));
@@ -34,7 +40,7 @@ $parsed_data = json_decode($data, true);
 
 $data = 
 	[
-	'speech'=> 'Test zzzz '.$_GET['getparam'].' '.$parsed_data['id'],
+	'speech'=> 'Test zzzz '.$_GET['getparam'].' '.$parsed_data['id'].json_encode($action).json_encode($parameters),
 	'source'=> 'apiai_webhook',
 	'displayText'=> 'Todayo '.$_GET['getparam'].' '.$parsed_data['id']
 	];
